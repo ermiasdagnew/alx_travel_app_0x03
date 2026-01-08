1a0cec593b1a9eb1f23239f14086005dc781afb8
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-08 13:54:30 +0300

    Update settings.py

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -1,15 +1,18 @@
+# Django settings for alx_travel_app
 
 INSTALLED_APPS = [
-    'django.contrib.contenttypes',
-    'django.contrib.auth',
-    'django_celery_results',
-    'listings',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django_celery_results",
+    "listings",
 ]
 
+# Celery configuration
 CELERY_BROKER_URL = "amqp://localhost"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_BACKEND = "django-db"
 
+# Email configuration
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DEFAULT_FROM_EMAIL = "no-reply@alxtravel.com"
